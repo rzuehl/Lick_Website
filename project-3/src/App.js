56203d2863934966484b7bf8ace52463caf7535c
rijul-kakar <rijulkakar1@gmail.com>
2023-10-25 17:03:17 -0500

    testing

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and testing vercel hosting
+          Edit <code>src/App.js</code> and testing vercel hosting 2
         </p>
         <a
           className="App-link"

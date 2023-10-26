1a0524abe7e2e74fe09cebb8ae9a9f36467acf22
Carlos Acosta <carlosra0345@gmail.com>
2023-10-26 01:59:06 -0500

    began work on menu board screen

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MenuBoard from './screens/MenuBoard.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and testing vercel hosting 2 TEST3 TEST 4 test_five Test-6
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MenuBoard />
     </div>
   );
 }

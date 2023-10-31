877b4e74cefc007a21ab1ddae52b605390592504
Ryan Zuehl <rzuehl@tamu.edu>
2023-10-31 17:02:27 -0500

    Implemented backend api server and SQL class

--- project-3/server/index.js
+++ project-3/server/index.js
@@ -1,1 +1,32 @@
-// main file for node backend
+//main file for node backend
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3001
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+});
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
+
+const sql = require("./SQL")
+
+app.get('/inventory', sql.getInventory)
+
--- project-3/src/screens/ManagerView.js
+++ project-3/src/screens/ManagerView.js
@@ -13,31 +13,45 @@ import HamburgerButton from '../components/HamburgerButton';
 import ScreenTitle from '../components/ScreenTitle';
 import weatherLogo from '../assets/weather-icon.png';
 import { Grid } from '@mui/material';
+import api from '../api/posts';
 
 
 function ManagerView() {
     // Connect to database here
-    const { Client } = require('pg')
-    const client = new Client({
-    user: 'csce315_907_03user',
-    host: 'csce-315-db.engr.tamu.edu',
-    database: 'csce315_907_03userdb',
-    password: 'GMV36DzB',
-    port: 5432,
-    })
-    client.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    });
+    // const { Client } = require('pg')
+    // const client = new Client({
+    // user: 'csce315_907_03user',
+    // host: 'csce-315-db.engr.tamu.edu',
+    // database: 'csce315_907_03userdb',
+    // password: 'GMV36DzB',
+    // port: 5432,
+    // })
+    // client.connect(function(err) {
+    // if (err) throw err;
+    // console.log("Connected!");
+    // });
 
     var buttonType = "manager"; 
 
     const handleInventoryManagement = () => {
         // document.getElementById("ManagerText").innerText = "Inventory Management";
-        const res =  client.query("SELECT * FROM inventory");
-        console.log(res);
-        
-            
+        // const res =  client.query("SELECT * FROM inventory");
+        // console.log(res);
+        const fetchInventory = async () => {
+            try {
+                //response returns a JSON format which can be accessed by response.data
+                const response = await api.get('/inventory');
+                let output = "";
+                for (let index = 0; index < Object.keys(response.data).length; index++) {
+                    let tempString = response.data[index].food_id + " " + response.data[index].food_type + " " + response.data[index].food_name + " " + response.data[index].quantity + " " + response.data[index].food_price + "\n";
+                    output += tempString;
+                }
+                document.getElementById("ManagerText").innerText = output;
+            } catch (err) {
+                console.log("FAIL");
+            }
+        }
+        fetchInventory();
     }
 
     const handleProductUsage = () => {

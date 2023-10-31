02a21e012bb5cc8c8c2ded38d835dcc5356b79ca
Your Name <carlosra0345@gmail.com>
2023-10-31 12:55:35 -0500

    updated login form to accept text input

--- project-3/src/screens/LoginView.js
+++ project-3/src/screens/LoginView.js
@@ -4,13 +4,16 @@
  * - General Button
  * - HamburgerButton
  * - ScreenTitle
+ * - GeneralInput
 */
 
 import React from 'react';
 import { Link } from 'react-router-dom';
 import GeneralButton from '../components/GeneralButton';
+import GeneralInput from '../components/GeneralInput';
 import ScreenTitle from '../components/ScreenTitle';
 import lickLogo from '../assets/lick-honest-logo.png';
+import weatherLogo from '../assets/weather-icon.png';
 
 function LoginView() {
     return (
@@ -20,19 +23,36 @@ function LoginView() {
                     <img className="lick-logo" src={lickLogo} alt="Representing Lick Honest Icecream Customer Logo" />                
                 </Link>
                 <GeneralButton content="Translate" sidePadding={35} /> 
+                <img className="weather-logo" src={weatherLogo} alt="Icon representing weather" />
                 <ScreenTitle />
-                <GeneralButton content="Color" sidePadding={20} />
+                <GeneralButton content="Login" sidePadding={20} route="/login" />
+                <GeneralButton content="Order" sidePadding={20} route="/menu" />
+                <GeneralButton content="Options" sidePadding={20} />
             </div>
 
+            <div className="login-main">
+                <div className="log-text">
+                    <h1>Sign In</h1>
+                </div>
+                <div className="log-body">
+                    <div className="log-body-content">
+                        <GeneralInput content="Email Address/Username" sidePadding={60} type="text" />
+                        <GeneralInput content="Password" sidePadding={60} type="password" />
+                        <GeneralButton content="Login" sidePadding={60} />
+                    </div>
+                </div>
+            </div>
+            {/*
             <div className="login-body">
-                <GeneralButton content="Email Address/Username" sidePadding={20} />
-                <GeneralButton content="Password" sidePadding={20} />
+                <GeneralInput content="Email Address/Username" sidePadding={20} type="text" />
+                <GeneralInput content="Password" sidePadding={20} type="password" />
                 <GeneralButton content="Login" sidePadding={20} />
             </div>
 
             <div className="login-text">
                 Sign In
             </div>
+            */}
         </div>
     );
 };

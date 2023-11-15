6b7058c50cc391921ab562e2a8de905baf6b8d93
StyrFryRyce <allenzhao1313@tamu.edu>
2023-11-15 09:59:14 -0600

    Changed the posts.js url to the development vercel url

--- project-3/src/api/posts.js
+++ project-3/src/api/posts.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://project-3-907-03.vercel.app/api'
+    baseURL: 'https://project-3-907-03-git-development-907-03.vercel.app/api/'
 });

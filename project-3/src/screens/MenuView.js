8e3d1e243070fe1018d70f7189406541f2a0882a
Your Name <carlosra0345@gmail.com>
2023-11-15 18:39:36 -0600

    worked on making menu-board dynamic

--- project-3/src/screens/MenuView.js
+++ project-3/src/screens/MenuView.js
@@ -218,55 +218,45 @@ const MenuView = () => {
 
     const fetchInventoryData = async () => {
         try {
+            const everydayData = [];
+            const dairyFreeVeganData =  [];
+            const seasonalData = [];
+            const sandwichData = [];
+            const toppingData = []
+            const beverageData = [];
+
             //response returns a JSON format which can be accessed by response.data
             const response = await api.get('/inventory');
             for(let entry of response.data) {
                 if (entry.food_type === "Ice Cream") {
-                    const everydayData = [
-                        ...everydayItems,
-                        entry
-                    ];
-                    setEverydayItems(everydayData);
+                    everydayData.push(entry);
                 }
                 else if (entry.food_type === "Dairy Free/Vegan") {
-                    const dairyFreeVeganData = [
-                        ...dairyFreeVeganItems,
-                        entry
-                    ];
-                    setDairyFreeVeganItems(dairyFreeVeganData);
+                    dairyFreeVeganData.push(entry);
                 }
                 else if (entry.food_type === "Seasonal Ice Cream") {
-                    const seasonalData = [
-                        ...seasonalItems,
-                        entry
-                    ];
-                    setSeasonalItems(seasonalData);
+                    seasonalData.push(entry);
                 }
                 else if (entry.food_type === "Sandwich") {
-                    const sandwichData = [
-                        ...sandwichItems,
-                        entry
-                    ];
-                    setSandwichItems(sandwichData);
+                    sandwichData.push(entry);
                 }
                 else if (entry.food_type === "Topping" || entry.food_type === "Seasonal Topping") {
-                    const toppingData = [
-                        ...toppingItems,
-                        entry
-                    ];
-                    setToppingItems(toppingData);
+                    toppingData.push(entry);
                 }
                 else if (entry.food_type === "Beverage") {
-                    const beverageData = [
-                        ...beverageItems,
-                        entry
-                    ];
-                    setBeverageItems(beverageData);
+                    beverageData.push(entry);
                 }
                 else {
                     throw new Error("Error: inventory item does not belong to defined food type");
                 }
             }
+            // setting values using useState
+            setEverydayItems(everydayData);
+            setDairyFreeVeganItems(dairyFreeVeganData);
+            setSeasonalItems(seasonalData);
+            setSandwichItems(sandwichData);
+            setToppingItems(toppingData);
+            setBeverageItems(beverageData);
         } 
         catch (err) {
             console.log("Error: Failed to query database to populate menu board", err);
@@ -285,13 +275,8 @@ const MenuView = () => {
                 <img className="weather-logo" src={weatherLogo} alt="Icon representing weather" />
                 <ScreenTitle />
                 <GeneralButton content="Login" sidePadding={20} route="/login" />
-<<<<<<< HEAD
                 <GeneralButton content="Cart" sidePadding={20} route="/cart" />
                 <GeneralButton content="Options" sidePadding={20} />
-=======
-                <GeneralButton content="Order" sidePadding={20} route="/menu" />
-                <OptionsDropdown sidePadding={20}/>
->>>>>>> 7733d4afd4d828a7e283128561e4a2bb745b0f6d
             </div>
             <div className="menu-categories">
                 <h2 id="sandwich-category" style={categoryIndex === 0 ? {textDecoration: 'underline'} : null} onClick={() => categoryClick(0)}>Sandwiches</h2>
@@ -306,10 +291,32 @@ const MenuView = () => {
                 {categoryIndex === 0 && (
                     <div className="menu-section">
                         <div className="menu-grid">
-                            <MenuTile src={caramelSandwich} alt="Image depicting Caramel ice cream sandwich" header="Caramel Salt Lick" description="Salty and sweet make it official! House made caramel ice cream is elevated by just a hint of sea salt then slathered between two soft chocolate cake cookies." />
-                            <MenuTile src={vanillaSandwich} alt="Image depicting Hill country Honey and Vanilla Bean ice cream sandwich" header="Vanilla Bean" description="Madagascar bourbon vanilla beans are enhanced by the sweet, complex notes of local Good Flow Honey in this classic ice cream sandwich." />
-                            <MenuTile src={vanillaSandwich} alt="Image depicting Vegan Vanilla ice cream sandwich" header="Vegan Vanilla" description="Our creamy coconut milk ice cream is accented with vanilla and sandwiched between two vegan & gluten free soft chocolate cake cookies." />
-                            <MenuTile src={chocolateSandwich} alt="Image depicting dark chocolate ice cream sandwich" header="Dark Chocolate" description="Buttery notes from SRSLY Chocolate and Texas Olive Ranch olive oil shine through our chocolate and sea salt ice cream which pair perfectly with decadent chocolate cake cookies" />
+                            {
+                                sandwichItems.map((item) => {
+                                    if (item.food_name in regularItemContent[item.food_type]) {
+                                        return (
+                                            <MenuTile 
+                                            src={regularItemContent[item.food_type][item.food_name]["image"]} 
+                                            alt={regularItemContent[item.food_type][item.food_name]["description"]}
+                                            header={item.food_name}
+                                            description={regularItemContent[item.food_type][item.food_name]["description"]}
+                                            itemPrice={item.food_price}
+                                            />
+                                        )
+                                    }
+                                    else {
+                                        return(
+                                            <MenuTile 
+                                            src={regularItemContent["Not Found"]["image"]} 
+                                            alt={regularItemContent["Not Found"]["description"]}
+                                            header={item.food_name}
+                                            description={regularItemContent["Not Found"]["description"]}
+                                            itemPrice={item.food_price}
+                                            />
+                                        )
+                                    }
+                                })
+                            }
                         </div>
                     </div>
                 )}
@@ -317,15 +324,32 @@ const MenuView = () => {
                 {categoryIndex === 1 && (
                     <div className="menu-section">
                         <div className="menu-grid">
-                            <MenuTile src={caramelSaltLick} alt="Image depicting caramel salt lick everyday icecream" header="Caramel Salt Lick" description="Salty and sweet make it official! House made caramel is elevated by just a hint of sea salt." />
-                            <MenuTile src={coffeeCream} alt="Image depicting everyday icecream Coffee with cream" header="Coffee with Cream" description="Milk & cream steeped in locally roasted Third Coast Coffee, lightly sweetened with pure cane syrup and subtly accented with a touch of Mexican vanilla." />
-                            <MenuTile src={darkChocolate} alt="Image depicting everyday icecream Dark Chocolate, olive oil, and sea salt" header="Dark Chocolate" description="Buttery notes from SRSLY Chocolate and Texas Olive Ranch olive oil shine through this dark chocolate and sea salt flavor." />
-                            <MenuTile src={mintChocolate} alt="Image depicting everyday icecream Fresh Mint and Chocolate Chunk" header="Mint & Chocolate Chunk" description="Fresh local mint is joined by chunks of house made SRSLY Chocolate ganache to perfect this crips & refreshing classic!" />
-                            <MenuTile src={goatCheese} alt="Image depicting everyday icecream Goat Cheese, Thyme and Honey" header="Goat Cheese" description="Fresh, local thyme and Good Flow honey pair perfectly with Austin's own creamy Pure Luck goat cheese." />
-                            <MenuTile src={honeyVanillaBean} alt="Image depicting everyday icecream Hill Country Honey and Vanilla Bean" header="Honey & Vanilla Bean" description="Madagascar bourbon vanilla beans are enhanced by the sweet, complex notes of local Good Flow Honey." />
-                            <MenuTile src={beetMint} alt="Image depicting everyday icecream Roasted Beet & Fresh Mint" header="Beets & Fresh Mint" description="Clean, crisp garden mint flawlessly complements the slightly sweet, earthy flavor of roasted Johnson's Backyard Garden beets." />
-                            <MenuTile src={milkChocolate} alt="Image depicting everyday icecream Milk Chocolate" header="Milk Chocolate" description="Flecks of local SRSLY Chocolate and the finest milk and cream are churned to classic perfection!" />
-                            <MenuTile src={sheetCake} alt="Image depicting everyday icecream Texas Sheet Cake" header="Texas Sheet Cake" description="Our interpretation of this classic cake combines a swirl of decadent chocolate-pecan icing with delicate chocolate ice cream." />
+                            {
+                                everydayItems.map((item) => {
+                                    if (item.food_name in regularItemContent[item.food_type]) {
+                                        return (
+                                            <MenuTile 
+                                            src={regularItemContent[item.food_type][item.food_name]["image"]} 
+                                            alt={regularItemContent[item.food_type][item.food_name]["description"]}
+                                            header={item.food_name}
+                                            description={regularItemContent[item.food_type][item.food_name]["description"]}
+                                            itemPrice={item.food_price}
+                                            />
+                                        )
+                                    }
+                                    else {
+                                        return(
+                                            <MenuTile 
+                                            src={regularItemContent["Not Found"]["image"]} 
+                                            alt={regularItemContent["Not Found"]["description"]}
+                                            header={item.food_name}
+                                            description={regularItemContent["Not Found"]["description"]}
+                                            itemPrice={item.food_price}
+                                            />
+                                        )
+                                    }
+                                })
+                            }
                         </div>
                     </div>
                 )}
@@ -343,8 +367,32 @@ const MenuView = () => {
                 {categoryIndex === 3 && (
                     <div className="menu-section">
                         <div className="menu-grid">
-                            <MenuTile src={veganSunbutter} alt="Image depicting gluten free/vegan item sunbutter" header="Honey Sunbutter" description="This late summer flavor is the perfect blend of sunflower seed SunButter and local Good Flow Honey." />
-                            <MenuTile src={veganVanillaPear} alt="Image depicting gluten free/vegan item vanilla pear" header="Vanilla Pear Cake" description="Smooth & creamy oat milk base is accented with vanilla, a silky swirl of our house made pear butter and bites of our luscious gluten free pear coffee cake." />
+                            {
+                                dairyFreeVeganItems.map((item) => {
+                                    if (item.food_name in regularItemContent[item.food_type]) {
+                                        return (
+                                            <MenuTile 
+                                            src={regularItemContent[item.food_type][item.food_name]["image"]} 
+                                            alt={regularItemContent[item.food_type][item.food_name]["description"]}
+                                            header={item.food_name}
+                                            description={regularItemContent[item.food_type][item.food_name]["description"]}
+                                            itemPrice={item.food_price}
+                                            />
+                                        )
+                                    }
+                                    else {
+                                        return(
+                                            <MenuTile 
+                                            src={regularItemContent["Not Found"]["image"]} 
+                                            alt={regularItemContent["Not Found"]["description"]}
+                                            header={item.food_name}
+                                            description={regularItemContent["Not Found"]["description"]}
+                                            itemPrice={item.food_price}
+                                            />
+                                        )
+                                    }
+                                })
+                            }
                         </div>
                     </div>   
                 )}
@@ -360,13 +408,32 @@ const MenuView = () => {
                 {categoryIndex === 5 && (
                     <div className="menu-section">
                         <div className="menu-grid">
-                            <MenuTile src={chocolateSauce} alt="Image depicting chocolate sauce topping" header="Chocolate Sauce" description="Our dark chocolate sauce is made from scratch in our kitchen with only four ingredients! It's a great addition to any of our flavors. (vegan, dairy free, wheat free)" />
-                            <MenuTile src={caramelSauce} alt="Image depicting caramel sauce topping" header="Caramel Sauce" description="Our rich caramel sauce is made from scratch in our kitchen with only four ingredients! It makes a great, sweet and salty addition to any flavor. (wheat free)" />
-                            <MenuTile src={toastedPecans} alt="Image depicting texas toasted pecan topping" header="Texas Toasted Pecans" description="exas pecans are the best pecans! We toast and chop local San Saba, TX pecans in our Northwest Austin kitchen. (vegan, wheat free)" />
-                            <MenuTile src={cookieCrumble} alt="Image depicting Chocolate Cookie Crumbles" header="Chocolate Cookie Crumble" description="Our take on our favorite crunchy chocolate cookies! This chocolatey, buttery crumble in made from scratch by our team. (non GMO)" />
-                            <MenuTile src={rainbowSprinkle} alt="Image depicting rainbow sprinkles" header="Rainbow Sprinkles" description="Classic sprinkles made with vibrant, plant-based colors by Color Kitchen! (non-GMO, wheat free)" />
-                            <MenuTile src={strawberry} alt="Image depicting the strawberry topping" header="Strawberry" description="Our fresh strawberry sauce is made from scratch in our kitchen only three ingredients including sweet local Texas strawberries!" />
-                            <MenuTile src={whippedCream} alt="Image depicting the whipped cream topping" header="Whipped Cream" description="Take your favorite ice cream to the next level with our sweet, whipped cream!" />
+                            {
+                                toppingItems.map((item) => {
+                                    if (item.food_name in regularItemContent[item.food_type]) {
+                                        return (
+                                            <MenuTile 
+                                            src={regularItemContent[item.food_type][item.food_name]["image"]} 
+                                            alt={regularItemContent[item.food_type][item.food_name]["description"]}
+                                            header={item.food_name}
+                                            description={regularItemContent[item.food_type][item.food_name]["description"]}
+                                            itemPrice={item.food_price}
+                                            />
+                                        )
+                                    }
+                                    else {
+                                        return(
+                                            <MenuTile 
+                                            src={regularItemContent["Not Found"]["image"]} 
+                                            alt={regularItemContent["Not Found"]["description"]}
+                                            header={item.food_name}
+                                            description={regularItemContent["Not Found"]["description"]}
+                                            itemPrice={item.food_price}
+                                            />
+                                        )
+                                    }
+                                })
+                            }
                         </div>
                     </div>                      
                 )}

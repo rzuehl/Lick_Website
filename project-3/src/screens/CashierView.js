a585775e21ed73ea90b1c1e0d950ecca5948fae3
StyrFryRyce <allenzhao1313@tamu.edu>
2023-11-29 23:36:24 -0600

    Added RemoveItems function

--- project-3/src/screens/CashierView.js
+++ project-3/src/screens/CashierView.js
@@ -4,27 +4,38 @@ import GeneralButton from '../components/GeneralButton';
 import OptionsDropdown from '../components/OptionsDropdown';
 import EmployeeButton from '../components/EmployeeButton.js';
 import ScreenTitle from '../components/ScreenTitle';
+import TableDropdown from '../components/TableDropdown.js';
 import weatherLogo from '../assets/weather-icon.png';
 import { Grid } from '@mui/material';
+
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TableFooter from '@mui/material/TableFooter';
 import Paper from '@mui/material/Paper';
-import api from '../api/posts';
+
 import { TableVirtuoso } from 'react-virtuoso';
+
+import api from '../api/posts';
+
 var isCategory = false;
 var category = "";
 var subtotal = 0;
 var tax = 0;
 var total = 0;
 var orderItemList = [];
+var selectedRows = [];
 var clicked = false;
 
 function createOrderItem(index, foodName, cost) {
     return {index, foodName, cost };
+}
+
+function createDropdownOptions(index, optionName, optionFunction) {
+  return {index, optionName, optionFunction};
 }
 
 const columns = [
@@ -40,28 +51,54 @@ const VirtuosoTableComponents = {
     <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
   ),
   TableHead,
-  TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
+  TableRow: ({ item: _item, ...props }) => <TableRow {...props} />, 
   TableBody: React.forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
+  TableFooter,
 };
+
 
 function CashierView() {
 
     var categoryItemArray = [];
     
+    
     const [categoryItemArrayState, setCategoryItemArrayState] = useState(categoryItemArray);
     const [orderItemListState, setOrderItemListState] = useState(orderItemList);
-    const [selectedRows, setSelectedRows] = useState([]);
+    const [selectedRowsState, setSelectedRows] = useState(selectedRows);
+
+    const removeItems = () =>{
+      const removedItemCosts = orderItemList.filter((item) => selectedRows.includes(item.index)).map((selectedItem) => selectedItem.cost);
+      for(let i = 0; i < removedItemCosts.length; i++){
+        subtotal -= removedItemCosts[i];
+      }
+      tax = subtotal * .05;
+      total = subtotal + tax;
+      document.getElementById('subtotal').innerText = subtotal.toFixed(2);
+      document.getElementById('tax').innerText = tax.toFixed(2);
+      document.getElementById('total').innerText = total.toFixed(2);
+      orderItemList = orderItemList.filter((item) => !selectedRows.includes(item.index));
+      
+      setOrderItemListState(orderItemList );
+      setSelectedRows([]);
+    };
+
+    
+
+    var dropdownOptionsArray = [];
+    dropdownOptionsArray.push(createDropdownOptions(0, "Remove Selected Items", removeItems));
 
     const handleRowClick = (id) => {
       setSelectedRows((prevSelectedRows) => {
-        console.log('Previous selected rows:', prevSelectedRows);
         if (id === -1) {
-          return (prevSelectedRows.includes(-1) && prevSelectedRows.length-1 === orderItemList.length) ? [] : [...orderItemList.map((_, index) => index), -1];
+          selectedRows = (prevSelectedRows.includes(-1) && prevSelectedRows.length-1 === orderItemList.length) ? [] : [...orderItemList.map((_, index) => index), -1]
+          return selectedRows;
         } else {
           if (prevSelectedRows.includes(id)) {
-            return prevSelectedRows.filter((rowId) => rowId !== id);
+            selectedRows = prevSelectedRows.filter((rowId) => rowId !== id)
+            return selectedRows;
           } else {
-            return [...prevSelectedRows, id];
+            selectedRows = [...prevSelectedRows, id];
+            return selectedRows;
           }
         }
       });
@@ -69,7 +106,7 @@ function CashierView() {
     
     const fixedHeaderContent = () => {
 
-      const isSelected = selectedRows.includes(-1);
+      const isSelected = selectedRowsState.includes(-1);
 
       return (
         <TableRow key={-1}>
@@ -80,7 +117,7 @@ function CashierView() {
           }}
           onClick={() => handleRowClick(-1)}
           >
-            <input type="checkbox" checked={isSelected}/>
+            <input type="checkbox" checked={isSelected} readOnly/>
           </TableCell>
           {columns.map((column) => (
             <TableCell
@@ -101,12 +138,12 @@ function CashierView() {
     
     const rowContent = (index, row) => {
 
-      const isSelected = selectedRows.includes(row['index']);
+      const isSelected = selectedRowsState.includes(row['index']);
 
       return (
         <React.Fragment key={row['index']}>
           <TableCell onClick={() => handleRowClick(row['index'])}>
-            <input type="checkbox" checked={isSelected}/>
+            <input type="checkbox" checked={isSelected} readOnly/>
           </TableCell>
           {columns.map((column) => (
             <TableCell
@@ -116,7 +153,31 @@ function CashierView() {
               {row[column.dataKey]}
             </TableCell>
           ))}
+
         </React.Fragment>
+      );
+    };
+
+    const fixedFooterContent = () =>{
+      return(
+        <TableRow key={-2}>
+          <TableCell 
+          sx={{
+              backgroundColor: 'background.paper',
+            }}/>
+          <TableCell
+          sx={{
+            backgroundColor: 'background.paper',
+          }}/>
+          <TableCell
+            sx={{
+              backgroundColor: 'background.paper',
+            }}
+            align="right"
+            >
+            <TableDropdown name="Options" options={dropdownOptionsArray}/>
+            </TableCell>
+        </TableRow>
       );
     };
 
@@ -133,12 +194,12 @@ function CashierView() {
                     subtotal += responseCost.data[0].food_price;
                     tax = subtotal * .05;
                     total = subtotal + tax;
-                    //document.getElementById('cashierText').innerText += eventString + " | " + responseCost.data[0].food_price + "\n";
-                    orderItemList.push(createOrderItem(orderItemList.length, eventString, responseCost.data[0].food_price));
-                    setOrderItemListState(orderItemList);
                     document.getElementById('subtotal').innerText = subtotal.toFixed(2);
                     document.getElementById('tax').innerText = tax.toFixed(2);
                     document.getElementById('total').innerText = total.toFixed(2);
+                    //document.getElementById('cashierText').innerText += eventString + " | " + responseCost.data[0].food_price + "\n";
+                    orderItemList.push(createOrderItem(orderItemList.length, eventString, responseCost.data[0].food_price));
+                    setOrderItemListState(orderItemList);
                 }
                 const responseCategories = await api.get('/category');
                 categoryItemArray = responseCategories.data.map(item => item.food_type);
@@ -205,7 +266,7 @@ function CashierView() {
                                 content={categoryItem}/>
                             </Grid>
                         ))}
-                    </Grid>
+                    </Grid> 
                     <Grid item xs={4}>
                     <Paper style={{ height: 400, width: '100%', marginBottom: '2%'}}>
                         <TableVirtuoso
@@ -213,7 +274,10 @@ function CashierView() {
                         components={VirtuosoTableComponents}
                         fixedHeaderContent={fixedHeaderContent}
                         itemContent={rowContent}
-                        />
+                        fixedFooterContent={fixedFooterContent}
+                        followOutput
+                        alignToBottom
+                        /> 
                     </Paper>
                         <div className='checkout-container'>
                           <div className='checkout-info'>
@@ -235,5 +299,7 @@ function CashierView() {
         </>
     )
 }
+
+
 
 export default CashierView;

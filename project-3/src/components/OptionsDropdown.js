8bfcbccc21ebb7c775bcdb66fa4219c9eccc4415
danielbarrett25 <daniel.barrett25@tamu.edu>
2023-11-30 15:55:35 -0600

    changes to OptionsDropdown; add weatherIcon to CustomerView

--- project-3/src/components/OptionsDropdown.js
+++ project-3/src/components/OptionsDropdown.js
@@ -3,9 +3,9 @@
 */
 
 import React from 'react';
-import GeneralButton from "../components/GeneralButton";
 import GeneralDropdown from '../components/GeneralDropdown';
 import { toggleStyle } from "../App"
+import { useState } from 'react';
 
 /**
  * OptionsDropdown is a custom component implementing GeneralDropdown with options specific to the Options menu
@@ -13,13 +13,36 @@ import { toggleStyle } from "../App"
  * @property {number} props.sidePadding - number setting side padding of button
  */
 function OptionsDropdown(props) {
+    // const [inputText, setInputText] = useState('');
+    // const [targetLanguage, setTargetLanguage] = useState('es'); // Default: Spanish
+
+    // const handleTranslate = async () => {
+    //     if (inputText) {
+    //         const translatedText = await translateText(inputText, targetLanguage);
+    //         console.log("translated text: " + translatedText);
+    //         // Do something with the translatedText, e.g., display it on the page.
+    //     }
+    // };
+
+    const google = window.google;
+
     return (
         <GeneralDropdown content="Options" sidePadding={props.sidePadding} 
             menu={[
-                <GeneralButton onClick={toggleStyle} content="High Contrast Mode" />
+                <OptionButton onclick={toggleStyle} content="High Contrast Mode" />,
+                
             ]}
         />
     );
+}
+
+// Translate API key:
+// AIzaSyAjsTytVyEbN9Ap81tqiuW8zVcoycN_oP4
+
+const OptionButton = ({onclick, content}) => {
+    return (
+        <a onClick={onclick}>{content}</a>
+    )
 }
 
 export default OptionsDropdown;
--- project-3/src/screens/CustomerView.js
+++ project-3/src/screens/CustomerView.js
@@ -11,6 +11,7 @@ import { Link } from 'react-router-dom';
 import GeneralButton from '../components/GeneralButton';
 import OptionsDropdown from '../components/OptionsDropdown';
 import ImageSlider from '../components/ImageSlider';
+import WeatherIcon from '../components/WeatherIcon';
 import ScreenTitle from '../components/ScreenTitle';
 import lickLogo from '../assets/lick-honest-logo.png';
 import weatherLogo from '../assets/weather-icon.png';
@@ -32,7 +33,8 @@ function CustomerView(props) {
                     <img className="lick-logo" src={lickLogo} alt="Representing Lick Honest Icecream Customer Logo" />                
                 </Link>
                 <GeneralButton content="Translate" sidePadding={35} />
-                <img className="weather-logo" src={weatherLogo} alt="Icon representing weather" />
+                {/* <img className="weather-logo" src={weatherLogo} alt="Icon representing weather" /> */}
+                <WeatherIcon />
                 <ScreenTitle />
                 <GeneralButton content="Login" sidePadding={20} route="/login" />
                 <GeneralButton content="Order" sidePadding={20} route="/menu" />
